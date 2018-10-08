8ed1c9cc8630a61f29b76297537f408db50aa762
Ayush <abansal1008@gmail.com>
2018-10-08 23:05:53 +0530

    Complete running pipeline

--- RBM.py
+++ RBM.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 
 class RBM:
-  
+
   def __init__(self, num_visible, num_hidden):
     self.num_hidden = num_hidden
     self.num_visible = num_visible
@@ -23,20 +23,20 @@ class RBM:
     self.weights = np.insert(self.weights, 0, 0, axis = 1)
 
   def train(self, data, max_epochs = 1000, learning_rate = 0.1):
-  
+
 
     num_examples = data.shape[0]
 
     # Insert bias units of 1 into the first column.
     data = np.insert(data, 0, 1, axis = 1)
 
-    for epoch in range(max_epochs):      
-      
-      pos_hidden_activations = np.dot(data, self.weights)      
+    for epoch in range(max_epochs):
+
+      pos_hidden_activations = np.dot(data, self.weights)
       pos_hidden_probs = self._logistic(pos_hidden_activations)
       pos_hidden_probs[:,0] = 1 # Fix the bias unit.
       pos_hidden_states = pos_hidden_probs > np.random.rand(num_examples, self.num_hidden + 1)
-   
+
       pos_associations = np.dot(data.T, pos_hidden_probs)
 
       neg_visible_activations = np.dot(pos_hidden_states, self.weights.T)
@@ -44,7 +44,7 @@ class RBM:
       neg_visible_probs[:,0] = 1 # Fix the bias unit.
       neg_hidden_activations = np.dot(neg_visible_probs, self.weights)
       neg_hidden_probs = self._logistic(neg_hidden_activations)
-      
+
       neg_associations = np.dot(neg_visible_probs.T, neg_hidden_probs)
 
       self.weights += learning_rate * ((pos_associations - neg_associations) / num_examples)
@@ -54,31 +54,31 @@ class RBM:
         print("Epoch %s: error is %s" % (epoch, error))
 
   def run_visible(self, data):
-    
-    
-    
+
+
+
     num_examples = data.shape[0]
-  
+
     hidden_states = np.ones((num_examples, self.num_hidden + 1))
-    
+
     # Insert bias units of 1 into the first column of data.
     data = np.insert(data, 0, 1, axis = 1)
 
-    
+
     hidden_activations = np.dot(data, self.weights)
-    
+
     hidden_probs = self._logistic(hidden_activations)
-    
+
     hidden_states[:,:] = hidden_probs > np.random.rand(num_examples, self.num_hidden + 1)
-   
-  
+
+
     # Ignore the bias units.
     hidden_states = hidden_states[:,1:]
     return hidden_states
-    
+
   # TODO: Remove the code duplication between this method and `run_visible`?
   def run_hidden(self, data):
-    
+
 
     num_examples = data.shape[0]
 
@@ -95,25 +95,25 @@ class RBM:
     visible_probs = self._logistic(visible_activations)
     # Turn the visible units on with their specified probabilities.
     visible_states[:,:] = visible_probs > np.random.rand(num_examples, self.num_visible + 1)
-  
+
     visible_states = visible_states[:,1:]
     return visible_states
-    
+
   def daydream(self, num_samples):
-   
+
     samples = np.ones((num_samples, self.num_visible + 1))
 
 
     samples[0,1:] = np.random.rand(self.num_visible)
 
-    
+
     for i in range(1, num_samples):
       visible = samples[i-1,:]
 
-      hidden_activations = np.dot(visible, self.weights)      
+      hidden_activations = np.dot(visible, self.weights)
 
       hidden_probs = self._logistic(hidden_activations)
-  
+
       hidden_states = hidden_probs > np.random.rand(self.num_hidden + 1)
       hidden_states[0] = 1
       visible_activations = np.dot(hidden_states, self.weights.T)
@@ -121,17 +121,17 @@ class RBM:
       visible_states = visible_probs > np.random.rand(self.num_visible + 1)
       samples[i,:] = visible_states
 
-    return samples[:,1:]        
-      
+    return samples[:,1:]
+
   def _logistic(self, x):
     return 1.0 / (1 + np.exp(-x))
 
-if __name__ == '__main__':
-  num_tags = 1000    #number of tags 
-  reduced_demension = 100 #reduced demension
-  epochs = 5000 
-  filename =""    # name of np array containing one hot encoding for classes
-  class_array =  np.load(filename)
+def train_RBM_and_compute_simiarity(class_array,reduced_dimension=100,epochs=5000):
+  #num_tags = 1000    #number of tags
+  #reduced_demension = 100 #reduced demension
+  #epochs = 5000
+  #filename =""    # name of np array containing one hot encoding for classes
+  num_tags = class_array.shape[1]
   r = RBM(num_visible = num_tags, num_hidden = reduced_demension)
   training_data = class_array
   r.train(training_data, max_epochs = epochs)
@@ -152,5 +152,4 @@ if __name__ == '__main__':
         simi_matrix[i][j] =val
         simi_matrix[j][i] = val
   np.save("similarity_matrix.npy",simi_matrix)
-
-  
+  return simi_matrix
---
+++ evaluate.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def compute_unseen_class_scores(seen_class_scores, K, seen_classes, unseen_classes):
+
+    return np.matmul(K[unseen_classes, :][:, seen_classes], seen_class_scores.T).T
+
+# computes precision@5
+def compute_precision(scores_gt, scores_pred):
+    precision = 0
+    num_questions = np.shape(scores_pred)[0]
+
+    for i in range(num_questions):
+        for j in np.argsort(scores_pred[i,:])[:5]:
+            if scores_gt[i, j]:
+                precision += 1
+
+    return precision/num_questions
